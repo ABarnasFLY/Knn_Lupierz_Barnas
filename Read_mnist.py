ede4f14d5486cb598e6eb4512f03fc343024c12a
Adrian Barnaś <abarnas@flytechuav.pl>
2017-11-26 19:00:47 +0100

    Read images with labels, rename mnist files

--- Read_mnist.py
+++ Read_mnist.py
@@ -1,20 +1,28 @@
-file = open('t10k-images.idx3-ubyte' , 'rb')
+file = open('test_images.byte' , 'rb')
+file_labels = open('test_labels.byte', 'rb')
+
 
 i = []
-
+images = []
+number = 20;
 file.read(46 +2*28)
-
+file_labels.read(8)
 n = 20
 
-for x in range(28*28*n):
-	i.append(file.read(1))
-
-for x in range(28*n):
-  for y in range(28):
-    d = int.from_bytes(i[x*28+y], byteorder='big')
-    if d > 50:
-      print('@' , end = ' ')
-    else:
-      print('~' , end = ' ')
-  print(' ')
+for p in range(number):
+  for x in range(28*28):
+    i.append(file.read(1))
+  i.append(int.from_bytes(file_labels.read(1), byteorder='big'))
+  images.append(i)
+  i =[]
+for p in range(number):
+  for x in range(28):
+    for y in range(28):
+      d = int.from_bytes(images[p][x*28+y], byteorder='big')
+      if d > 50:
+        print('@' , end = ' ')
+      else:
+        print('~' , end = ' ')
+    print(' ')
+  print(images[p][-1])
 file.close()
