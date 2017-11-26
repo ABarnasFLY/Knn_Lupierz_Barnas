90603025ae9699246f81815f10b904bb72295945
Adrian Barnaś <abarnas@flytechuav.pl>
2017-11-26 17:43:00 +0100

    measure euclidean distance

--- Knn_iris.py
+++ Knn_iris.py
@@ -1,6 +1,7 @@
 import csv
 import random
-	
+import math
+
 def loadDataset(filename, split, trainingSet=[], testSet=[]):
   with open(filename, 'r') as csvfile:
     lines = csv.reader(csvfile)
@@ -13,8 +14,22 @@ def loadDataset(filename, split, trainingSet=[], testSet=[]):
       else:
         testSet.append(dataset[x])
 
+def euclideanDistance(instance1, instance2, length):
+  distance = 0
+  for x in range(length):
+    distance += pow((instance1[x] - instance2[x]),2)
+  return math.sqrt(distance)
+
+data1 = [2,2,2,'a']
+data2 = [4,4,4,'b']
+
+distance = euclideanDistance(data1, data2, 3)
+print('Distance = ' + repr(distance))
+
 trainingSet = []
 testSet = []
 loadDataset('iris.data.txt', 0.66, trainingSet, testSet)
 print('Train ' + repr(len(trainingSet)))
 print('Test ' + repr(len(testSet)))
+
+
