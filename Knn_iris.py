6c4bea37315d399e703deeaa1035494e2aa4be03
Adrian Barnaś <abarnas@flytechuav.pl>
2017-11-26 18:04:25 +0100

    Select major vote

--- Knn_iris.py
+++ Knn_iris.py
@@ -33,6 +33,22 @@ def getNeighbors(trainingSet, testInstance, k):
     neighbors.append(distances[x][0])
   return neighbors
 
+def getResponse(neighbors):
+  classVotes = {}
+  for x in range(len(neighbors)):
+    response = neighbors[x][-1]
+    if response in classVotes:
+      classVotes[response] += 1
+    else:
+      classVotes[response] = 1
+  sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse = True)
+  return sortedVotes[0][0]
+  
+neighbors = [[1,1,1,'a'], [2,2,2,'a'], [3,3,3,'b']]
+response = getResponse(neighbors)
+print(response)
+  
+
 trainSet = [[2, 2, 2, 'a'], [4, 4, 4, 'b']]
 testInstance = [5, 5, 5]
 k = 1
