6b3bb20f1d3ec030e27f3cbd7a70c1f790c017cb
Adrian Barnaś <abarnas@flytechuav.pl>
2017-11-26 17:33:56 +0100

    Fix indent bugs, print datasets length, change open file mode

--- Knn_iris.py
+++ Knn_iris.py
@@ -1,18 +1,20 @@
-#import csv
-#import random
-#with open('iris.data.txt') as csvfile:
-#  lines = csv.reader(csvfile)
-#  for row in lines:
-#    print(', '.join(row))
+import csv
+import random
 	
 def loadDataset(filename, split, trainingSet=[], testSet=[]):
-  with open(filename, 'rb') as csvfile:
+  with open(filename, 'r') as csvfile:
     lines = csv.reader(csvfile)
-	dataset = list(lines)
-	for x in range(len(dataset)-1):
-	  for y in range(4):
-	    dataset[x][y] = float(dataset[x][y])
-	  if random.random() < split:
-	    trainingSet.append(dataset[x])
-	  else:
-	    testSet.append(dataset[x])
+    dataset = list(lines)
+    for x in range(len(dataset)-1):
+      for y in range(4):
+        dataset[x][y] = float(dataset[x][y])
+      if random.random() < split:
+        trainingSet.append(dataset[x])
+      else:
+        testSet.append(dataset[x])
+
+trainingSet = []
+testSet = []
+loadDataset('iris.data.txt', 0.66, trainingSet, testSet)
+print('Train ' + repr(len(trainingSet)))
+print('Test ' + repr(len(testSet)))
