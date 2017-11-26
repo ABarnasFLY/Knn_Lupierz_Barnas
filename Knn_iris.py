07e99c8746d2076f7f3f8aa52c2b9e1e81e1db88
Adrian Barnaś <abarnas@flytechuav.pl>
2017-11-26 17:27:40 +0100

    Definition of read function

--- Knn_iris.py
+++ Knn_iris.py
@@ -1,5 +1,18 @@
-import csv
-with open('iris.data.txt') as csvfile:
-  lines = csv.reader(csvfile)
-  for row in lines:
-    print(', '.join(row))
+#import csv
+#import random
+#with open('iris.data.txt') as csvfile:
+#  lines = csv.reader(csvfile)
+#  for row in lines:
+#    print(', '.join(row))
+	
+def loadDataset(filename, split, trainingSet=[], testSet=[]):
+  with open(filename, 'rb') as csvfile:
+    lines = csv.reader(csvfile)
+	dataset = list(lines)
+	for x in range(len(dataset)-1):
+	  for y in range(4):
+	    dataset[x][y] = float(dataset[x][y])
+	  if random.random() < split:
+	    trainingSet.append(dataset[x])
+	  else:
+	    testSet.append(dataset[x])
